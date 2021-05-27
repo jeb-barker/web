430dff5c3b2bb1bff3c80ecebeff6b072abfc3ec
jeebtheweeb <48389379+jeebtheweeb@users.noreply.github.com>
2021-05-26 20:12:08 -0700

    APOD

--- routes/apod.js
+++ routes/apod.js
@@ -1,12 +1,21 @@
+let dat = "";
+
 function input(req, res, next){
     https.get(url, options, function(response){
         response.on('data', function(chunk){
-            let dat = "";
-            dat
+            dat+=chunk;
+            console.log("DAT= " + dat)
+        })
+        response.on('end', function(){
+            res.locals.inJSON = JSON.parse(dat);
+            next(req, res);
         })
     })
 }
 
+function display(req, res){
+    res.json(res.locals.inJSON);
+}
 
 module.exports.run_setup = function(app){
     app.get('/apod', [input, display]);
