ced8443e10183b408f0e8833a288de25e6dfd3a7
jeebtheweeb <jeb.e.barker@gmail.com>
2021-06-29 19:52:07 -0400

    added move list on side of chess site

--- routes/css.js
+++ routes/css.js
@@ -9,5 +9,35 @@ module.exports.run_setup = function(app){
         else if (req.query.name == "facts") {
             res.sendFile(__dirname + "/css/facts.css");
         }
+        else if (req.query.name == "cm"){
+            res.sendFile(__dirname + "/css/styles/cm-chessboard.css");
+        }
+        else if (req.query.name == "chb"){
+            res.sendFile("/site/node_modules/cm-chessboard/src/cm-chessboard/Chessboard.js");
+        }
+        else if (req.query.name == "cmi"){
+            res.sendFile("/site/node_modules/cm-chessboard/src/cm-chessboard/ChessboardMoveInput.js");
+        }
+        else if (req.query.name == "cpa"){
+            res.sendFile("/site/node_modules/cm-chessboard/src/cm-chessboard/ChessboardPiecesAnimation.js");
+        }
+        else if (req.query.name == "cs"){
+            res.sendFile("/site/node_modules/cm-chessboard/src/cm-chessboard/ChessboardState.js");
+        }
+        else if (req.query.name == "cv"){
+            res.sendFile("/site/node_modules/cm-chessboard/src/cm-chessboard/ChessboardView.js");
+        }
+        else if (req.query.name == "staunty"){
+            res.sendFile("/site/node_modules/cm-chessboard/assets/images/chessboard-sprite-staunty.svg");
+        }
+        else if (req.query.name == "cssm"){
+            res.sendFile("/site/node_modules/cm-chessboard/styles/cm-chessboard.css.map");
+        }
+        else if (req.query.name == "scss"){
+            res.sendFile("/site/node_modules/cm-chessboard/styles/cm-chessboard.scss");
+        }
+        else if (req.query.name == "chesscss") {
+            res.sendFile(__dirname + "/css/chesscss.css");
+        }
     });
 }
--- routes/jebchess.js
+++ routes/jebchess.js
@@ -4,6 +4,8 @@ var https = require('https');
 
 module.exports.run_setup = function(app){
     
+    
+    
     app.get('/jebchess', function (req, res) {
         res.render('jebchess.hbs', {})
     })
